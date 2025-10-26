76403c8b98bc830668eda871f28a4d7c067d7116
grantcrawford <grantcrawford@users.noreply.github.com>
2025-10-26 15:02:25 +1100

    Update gnome-hotkeys.sh
    Update super keys for workspaces 7 to 9

--- gnome/gnome-hotkeys.sh
+++ gnome/gnome-hotkeys.sh
@@ -20,6 +20,9 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>7']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>8']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>9']"
 
 # Alt+F4 is very cumbersome
 gsettings set org.gnome.desktop.wm.keybindings close "['<Super>w']"
